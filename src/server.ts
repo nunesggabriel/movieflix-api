5853e29d61cd3425808d0f320b9a237d4fefd711
Gabriel Nunes <gabrielnunes25@icloud.com>
2026-02-25 19:53:52 -0300

    adicionada ordenação, gênero e linguagem do filme

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 
 const port = 3000;
@@ -7,8 +7,16 @@ const app = express();
 const prisma = new PrismaClient();
 
 
-app.get('/movies', async (req, res) => {
-  const movies = await prisma.movie.findMany();
+app.get('/movies', async (_, res) => {
+  const movies = await prisma.movie.findMany({
+    orderBy: {
+      title: "asc",
+          },
+          include: {
+            genres: true,
+            languages: true,
+          },
+        });
   res.json(movies);
 });
 
