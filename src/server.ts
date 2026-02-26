975a22d7024b269b66a7aa94525872f0c5dbc4a3
Gabriel Nunes <gabrielnunes25@icloud.com>
2026-02-26 20:40:04 -0300

    Adicionada atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -53,6 +53,33 @@ app.post("/movies", async (req, res) => {
 res.status(201).send();
 });
 
+app.put("/movies/:id", async (req, res) => {
+  
+  const id = Number(req.params.id);
+
+  try{
+  const movie = await prisma.movie.findUnique({
+    where: { id },
+  });
+  if (!movie) {return res.status(404).send({ message: "Filme não encontrado" });
+}
+
+  const data= {...req.body};
+  data.release_date = data.release_date ? new Date(data.release_date) : undefined;
+
+  await prisma.movie.update({
+    where: { id },
+    data: data,
+  });
+  }catch (error) {
+return res.status(500).send({ message: "Erro ao atualizar o filme" });
+  }
+
+  res.status(200).send();
+
+
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor em execução http://localhost:${port}`);
