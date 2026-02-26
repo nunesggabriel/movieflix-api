4794f945a461c2bbc27c0ab4ef871d9e5d61f6e2
Gabriel Nunes <gabrielnunes25@icloud.com>
2026-02-26 19:23:39 -0300

    Adicionada mensagem de erro na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { PrismaClient } from "@prisma/client";
+import console = require('node:console');
 
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
+
+app.use(express.json());
 
 
 app.get('/movies', async (_, res) => {
@@ -19,6 +22,28 @@ app.get('/movies', async (_, res) => {
         });
   res.json(movies);
 });
+
+app.post("/movies", async (req, res) => {
+
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+  try {
+  await prisma.movie.create({
+    data: {
+     title,
+     genre_id,
+     language_id,
+     oscar_count,
+     release_date: new Date(release_date)
+  }
+});
+}catch (error) {
+  return res.status(500).json({ message: "Erro ao criar o filme" });
+}
+
+res.status(201).send();
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor em execução http://localhost:${port}`);
